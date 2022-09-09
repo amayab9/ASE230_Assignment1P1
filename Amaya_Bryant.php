5c7a440ef287311bc460657dc7ad7bae339efa9d
Amaya <bryanta21@nku.edu>
2022-09-09 13:12:00 -0400

    This is the updated version with corrected html tags/php updates

---
+++ Amaya_Bryant.php
@@ -0,0 +1,100 @@
+<!doctype html>
+  <html lang="en">
+    <head>
+      <!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+      <link rel="stylesheet" href="assets/css/detail.css" />
+		  <title>ASE 230 - <?php echo 'Amaya Bryant' ?></title>
+    </head>
+		
+	  <body>
+
+      <div class="container text-center mb-5">
+        <h1>This is ASE 230 - <?php echo 'Amaya Bryant' ?></h1>
+      </div>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5 col-md-6">
+            <div class="mb-2">
+              <img class="w-100" src="images/amaya1.jpg" alt="Amaya Bryant">
+            </div>
+            <div class="mb-2 d-flex">
+              <h4 class="font-weight-normal"><?php echo 'Amaya Bryant' ?></h4>
+              <div class="social d-flex ml-auto">
+                <p class="pr-2 font-weight-normal">Follow on:</p>
+                <a href="#" class="text-muted mr-1">
+                  <i class="fab fa-facebook-f"></i>
+                </a>
+                <a href="#" class="text-muted mr-1">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#" class="text-muted mr-1">
+                  <i class="fab fa-instagram"></i>
+                </a>
+                <a href="#" class="text-muted">
+                  <i class="fab fa-linkedin"></i>
+                </a>
+              </div>
+            </div>
+            <div class="mb-2">
+              <ul class="list-unstyled">
+                <li class="media">
+                  <span class="w-25 text-black font-weight-normal">Dream profession:</span>
+                  <label class="media-body"><?php echo 'Software Developer' ?></label>
+                </li>
+                <li class="media">
+                  <span class="w-25 text-black font-weight-normal">Dream company: </span>
+                  <label class="media-body"><?php echo 'Google or Microsoft' ?></label>
+                </li>
+                <li class="media">
+                  <span class="w-25 text-black font-weight-normal">Email: </span>
+                  <label class="media-body"><?php echo 'bryanta21@nku.edu' ?></label>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-7 col-md-6 pl-xl-3">
+            <h5 class="font-weight-normal">Short intro</h5>
+            <p><?php echo 'My name is Amaya Bryant and I am a Sophomore at NKU. I am majoring in ASE, with a minor in Computer Science. I am the oldest of 3 and I do not have many hobbies other than playing video games and talking to friends.' ?></p>
+            <div class="my-2 bg-light p-2">
+              <p class="font-italic mb-0"><?php echo 'Oh the thinks you can think. - Dr. Seuss' ?></p>
+            </div>
+            <div class="mb-2 mt-2 pt-1">
+              <h5 class="font-weight-normal">Top skills</h5>
+            </div>
+            <div class="py-1">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width:85%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar-title"><?php echo 'Being bored' ?></div>
+                  <span class="progress-bar-number"><?php echo '85%' ?></span>
+                </div>
+              </div>
+            </div>
+            <div class="py-1">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width:35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar-title"><?php echo 'Programming' ?></div>
+                  <span class="progress-bar-number"><?php echo '35%' ?></span>
+                </div>
+              </div>
+            </div>
+            <div class="py-1">
+              <div class="progress">
+                <div class="progress-bar" role="progressbar" style="width:90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100">
+                  <div class="progress-bar-title"><?php echo 'Sleeping' ?></div>
+                  <span class="progress-bar-number"><?php echo '90%' ?></span>
+                </div>
+              </div>
+            </div>
+            <h5 class="font-weight-normal">Fun fact</h5>
+              <p><?php echo 'I recently started getting into collecting Nintendo products.' ?> <a href="index.php"> Back</a></p>
+            </div>
+          </div>
+        </div> 
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    </body>
+</html>
